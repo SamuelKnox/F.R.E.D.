ddeeb2cddbcdc0e2e50fefb2a839d804ed00229f
Evorlor <Evorlor@gmail.com>
2014-02-21 12:45:05 -0500

    initial commit

---
+++ Mascape/Attribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mascape
+{
+    class Attribute
+    {
+        public double Current { get; set; }
+        public double Maximum { get; set; }
+
+                public Attribute(double max)
+        {
+            Current = max;
+            Maximum = max;
+        }
+    }
+}
---
+++ Mascape/Trap.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mascape
+{
+    class Trap
+    {
+        public double Damage { get; set; }
+        public double Delay { get; set; }
+        public Texture2D Image { get; set; }
+        public Rectangle Rectangle { get; set; }
+        public bool Active { get; set; }
+
+
+        public Trap(double dmg, double delay)
+        {
+            Damage = dmg;
+            Delay = delay;
+            Active = true;
+        }
+    }
+}
