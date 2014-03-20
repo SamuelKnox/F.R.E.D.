d85ea00eae6ffeb7dde17455640496ee1f39e520
Evorlor <Evorlor@gmail.com>
2014-03-20 13:14:05 -0400

    added movement
    Conflicts:
    	Fred.v11.suo
    	Fred/bin/WindowsGL/Debug/Fred.exe
    	Fred/bin/WindowsGL/Debug/Fred.pdb
    	Fred/obj/x86/Debug/Fred.csprojResolveAssemblyReference.cache
    	Fred/obj/x86/Debug/Fred.exe
    	Fred/obj/x86/Debug/Fred.pdb

---
+++ Fred/Components/VelocityComponent.cs
@@ -0,0 +1,44 @@
+﻿using Artemis.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fred.Components
+{
+    class VelocityComponent : IComponent
+    {
+        private const float ToRadians = (float)(Math.PI / 180.0);
+
+        public float Speed { get; set; }
+        public float Angle { get; set; }
+
+        public float AngleAsRadians
+        {
+            get
+            {
+                return this.Angle * ToRadians;
+            }
+        }
+
+        public VelocityComponent()
+            : this(0.0f, 0.0f)
+        {
+        }
+
+        public VelocityComponent(float velocity)
+            : this(velocity, 0.0f)
+        {
+        }
+
+        public VelocityComponent(float velocity, float angle)
+        {
+            this.Speed = velocity;
+            this.Angle = angle;
+        }
+        public void AddAngle(float angle)
+        {
+            this.Angle = (this.Angle + angle) % 360;
+        }
+    }
+}
