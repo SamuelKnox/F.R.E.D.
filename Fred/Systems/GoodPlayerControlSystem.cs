0c62271614e6a61412171e1dcb07d5d1afa256be
Evorlor <Evorlor@gmail.com>
2014-03-21 12:46:06 -0400

    addded controllerablenessability

--- Fred/Systems/GoodPlayerControlSystem.cs
+++ Fred/Systems/GoodPlayerControlSystem.cs
@@ -32,6 +32,7 @@ namespace Fred.Systems
 
         public override void Process(Entity entity)
         {
+            // TODO: Add your update logic here
             TransformComponent transformComponent = entity.GetComponent<TransformComponent>();
             VelocityComponent velocityComponent = entity.GetComponent<VelocityComponent>();
 
@@ -41,111 +42,107 @@ namespace Fred.Systems
             float keyMoveSpeed = 0.001F * TimeSpan.FromTicks(this.EntityWorld.Delta).Milliseconds;
             float moveSpeedFriction = 0.0003f * TimeSpan.FromTicks(this.EntityWorld.Delta).Milliseconds;
 
-            Keys[] pressed_Key = Keyboard.GetState().GetPressedKeys();
+            KeyboardState pressedKey = Keyboard.GetState();
+            GamePadState controller = GamePad.GetState(PlayerIndex.One);
 
-            for (int i = 0; i < pressed_Key.Length; i++)
+            if (pressedKey.IsKeyDown(Keys.A) || controller.ThumbSticks.Left.X < 0)
             {
-                switch (pressed_Key[i])
+                if (Math.Abs(velocityComponent.Speed) < turningOffset)
                 {
+                    velocityComponent.Angle = 180;
+                }
 
-                    case Keys.A:
-                        if (Math.Abs(velocityComponent.Speed) < turningOffset)
-                        {
-                            velocityComponent.Angle = 180;
-                        }
+                if (velocityComponent.Angle >= 90 && velocityComponent.Angle < 270)
+                {
+                    velocityComponent.Speed += keyMoveSpeed;
+                }
+                else
+                {
+                    velocityComponent.Speed -= keyMoveSpeed;
+                }
 
-                        if (velocityComponent.Angle >= 90 && velocityComponent.Angle < 270)
-                        {
-                            velocityComponent.Speed += keyMoveSpeed;
-                        }
-                        else
-                        {
-                            velocityComponent.Speed -= keyMoveSpeed;
-                        }
-
-                        if (velocityComponent.Angle < 180)
-                        {
-                            velocityComponent.AddAngle(changeInAngle);
-                        }
-                        else if (velocityComponent.Angle > 180 && velocityComponent.Angle > 0)
-                        {
-                            velocityComponent.AddAngle(-changeInAngle);
-                        }
-                        break;
-
-                    case Keys.D:
-                        if (Math.Abs(velocityComponent.Speed) < turningOffset)
-                        {
-                            velocityComponent.Angle = 0;
-                        }
-                        if (velocityComponent.Angle >= 270 || velocityComponent.Angle < 90)
-                        {
-                            velocityComponent.Speed += keyMoveSpeed;
-                        }
-                        else
-                        {
-                            velocityComponent.Speed -= keyMoveSpeed;
-                        }
-                        if (velocityComponent.Angle > 180)
-                        {
-                            velocityComponent.AddAngle(changeInAngle);
-                        }
-                        else if (velocityComponent.Angle < 180 && velocityComponent.Angle > 0)
-                        {
-                            velocityComponent.AddAngle(-changeInAngle);
-                        }
-                        break;
-
-                    case Keys.W:
-                        if (Math.Abs(velocityComponent.Speed) < turningOffset)
-                        {
-                            velocityComponent.Angle = 270;
-                        }
-                        if (velocityComponent.Angle >= 180)
-                        {
-                            velocityComponent.Speed += keyMoveSpeed;
-                        }
-                        else
-                        {
-                            velocityComponent.Speed -= keyMoveSpeed;
-                        }
-                        if (velocityComponent.Angle < 270 && velocityComponent.Angle > 90)
-                        {
-                            velocityComponent.AddAngle(changeInAngle);
-                        }
-                        else if (velocityComponent.Angle < 90 || velocityComponent.Angle > 270)
-                        {
-                            velocityComponent.AddAngle(-changeInAngle);
-                        }
-                        break;
-
-                    case Keys.S:
-                        if (Math.Abs(velocityComponent.Speed) < turningOffset)
-                        {
-                            velocityComponent.Angle = 90;
-                        }
-                        if (velocityComponent.Angle < 180)
-                        {
-                            velocityComponent.Speed += keyMoveSpeed;
-                        }
-                        else
-                        {
-                            velocityComponent.Speed -= keyMoveSpeed;
-                        }
-                        if (velocityComponent.Angle < 90 || velocityComponent.Angle > 270)
-                        {
-                            velocityComponent.AddAngle(changeInAngle);
-                        }
-                        else if (velocityComponent.Angle < 270 && velocityComponent.Angle > 90)
-                        {
-                            velocityComponent.AddAngle(-changeInAngle);
-                        }
-                        break;
-
-                    default:
-                        break;
+                if (velocityComponent.Angle < 180)
+                {
+                    velocityComponent.AddAngle(changeInAngle);
+                }
+                else if (velocityComponent.Angle > 180 && velocityComponent.Angle > 0)
+                {
+                    velocityComponent.AddAngle(-changeInAngle);
                 }
             }
+            if (pressedKey.IsKeyDown(Keys.D) || controller.ThumbSticks.Left.X > 0)
+            {
+                if (Math.Abs(velocityComponent.Speed) < turningOffset)
+                {
+                    velocityComponent.Angle = 0;
+                }
+                if (velocityComponent.Angle >= 270 || velocityComponent.Angle < 90)
+                {
+                    velocityComponent.Speed += keyMoveSpeed;
+                }
+                else
+                {
+                    velocityComponent.Speed -= keyMoveSpeed;
+                }
+                if (velocityComponent.Angle > 180)
+                {
+                    velocityComponent.AddAngle(changeInAngle);
+                }
+                else if (velocityComponent.Angle < 180 && velocityComponent.Angle > 0)
+                {
+                    velocityComponent.AddAngle(-changeInAngle);
+                }
+            }
+
+            if (pressedKey.IsKeyDown(Keys.W) || controller.ThumbSticks.Left.Y > 0)
+            {
+                if (Math.Abs(velocityComponent.Speed) < turningOffset)
+                {
+                    velocityComponent.Angle = 270;
+                }
+                if (velocityComponent.Angle >= 180)
+                {
+                    velocityComponent.Speed += keyMoveSpeed;
+                }
+                else
+                {
+                    velocityComponent.Speed -= keyMoveSpeed;
+                }
+                if (velocityComponent.Angle < 270 && velocityComponent.Angle > 90)
+                {
+                    velocityComponent.AddAngle(changeInAngle);
+                }
+                else if (velocityComponent.Angle < 90 || velocityComponent.Angle > 270)
+                {
+                    velocityComponent.AddAngle(-changeInAngle);
+                }
+            }
+
+            if (pressedKey.IsKeyDown(Keys.S) || controller.ThumbSticks.Left.Y < 0)
+            {
+                if (Math.Abs(velocityComponent.Speed) < turningOffset)
+                {
+                    velocityComponent.Angle = 90;
+                }
+                if (velocityComponent.Angle < 180)
+                {
+                    velocityComponent.Speed += keyMoveSpeed;
+                }
+                else
+                {
+                    velocityComponent.Speed -= keyMoveSpeed;
+                }
+                if (velocityComponent.Angle < 90 || velocityComponent.Angle > 270)
+                {
+                    velocityComponent.AddAngle(changeInAngle);
+                }
+                else if (velocityComponent.Angle < 270 && velocityComponent.Angle > 90)
+                {
+                    velocityComponent.AddAngle(-changeInAngle);
+                }
+            }
+
+
 
             // Handle max speed
             if (velocityComponent.Speed > 0)
